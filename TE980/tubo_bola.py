c03287e459e45cc8289bd4f3ba6fa5a84100d83a
Caio P. Mizerkowski <caiomizerkowski@gmail.com>
2022-08-28 15:09:26 -0300

    🚧: trabalho 5 parcialmente feito made by Caio Mizerkowski for college

--- TE980/tubo_bola.py
+++ TE980/tubo_bola.py
@@ -47,6 +47,10 @@ def distance():
     distance = (TimeElapsed * 34300) / 2
     return distance
 
+0.809
+0.901
+0.969
+
 
 control = controler(A)
 fan = gpio.PWM(pin_out, frequency)
@@ -54,34 +58,36 @@ fan.start(start_dc)
 
 if identificacao:
     print("Identificação")
-    min_erro = 100
+    min_erro = 10000000
     A_min = 0
-
     for i in range(0, 50):
         A = 2 * i / (50 + i)
         control = controler(A)
         erros = []
+        print(f'A={A}')
+        try:
+            for k in range(200):
+                value = distance()
+                update = control.controlador(value, target)
+                fan.ChangeDutyCycle(update)
+                erros.append(target - value)
+                sleep(2 / frequency)
 
-        for k in range(200):
-            value = distance()
-            update = control.controlador(value, target)
-            fan.ChangeDutyCycle(max(min(update, 100), 0))
-            erros.append(target - value)
-            sleep(2 / frequency)
+            if np.abs(min_erro) > np.abs(np.mean(erros[100:])):
+                A_min = A
+                min_erro = np.mean(erros[100:])
+                print(f"Novo menor erro: {min_erro:.03f} com A = {A_min:0.03f}")
+                if np.abs(min_erro) < 0.01:
+                    break
 
-        if np.abs(min_erro) > np.abs(np.mean(erros[100:])):
-            A_min = A
-            min_erro = np.mean(erros[100:])
-            print(f"Novo menor erro: {min_erro:.03f} com A = {A_min:0.03f}")
-            if np.abs(min_erro) < 0.01:
-                break
-
-        fan.ChangeDutyCycle(start_dc)
-        sleep(1)
-
+            fan.ChangeDutyCycle(start_dc)
+            sleep(1)
+        except Exception as e:
+            print('interrompido')
     print(f"A_min: {A_min:.03f} erro: {min_erro:.02f}")
     A = A_min
     _ = input(f"Sistema identificado com A = {A:.04f}")
+
 
 control = controler(A)
 try:
